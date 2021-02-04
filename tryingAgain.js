d6344212506ad3b5fb0655c9d3dad29b7d80f434
Na Wei <na.wei95@hotmail.com>
2021-02-04 11:09:56 +0100

    getting verses

--- tryingAgain.js
+++ tryingAgain.js
@@ -10,6 +10,10 @@ const getHref = (parseHtml, html, array) => {
 };
 const eachBook = ["#testament-O > div > a", "#testament-N > div > a"];
 const eachChapter = ["#content-column > div > div > div > div > div > a"];
+const eachVerse = [
+  "#content-column > div.row.bible-container > div > div > div > div > .verse > .verse-number",
+  "#content-column > div.row.bible-container > div > div > div > div > .verse",
+];
 
 const getData = (url, parseHtml, getHref) =>
   rp(url)
@@ -22,9 +26,30 @@ const getData = (url, parseHtml, getHref) =>
       console.log("error:", err);
     });
 
-getData(baseURL + bibleVersion, eachBook, getHref).then((allBookURLS) => {
-  const allChapters = allBookURLS.map(async (bookURL) => {
-    return await getData(bookURL, eachChapter, getHref).then((data) => data);
+// getData(baseURL + bibleVersion, eachBook, getHref).then((allBookURLS) => {
+//   const allChapters = allBookURLS.map(async (bookURL) => {
+//     return await getData(bookURL, eachChapter, getHref).then((allChapterURLS) => {
+//       return await getData(allChapterURLS) // working on this part
+//     });
+//   });
+//   Promise.all(allChapters).then((chapters) => console.log(chapters[0].length)); //Array of books with array of chapters
+// });
+const mockURL = "https://www.biblestudytools.com/cuvs/shipian/1.html";
+rp(mockURL).then((html) => {
+  const vNum = cheerio(eachVerse[0], html).text();
+  const verseNumbers = vNum.split("");
+  const rawVerses = [];
+  verseNumbers.map((verseNumber) => {
+    const verseHtmlPath = `${eachVerse[1]} > .verse-${verseNumber} `;
+    const verse = cheerio(verseHtmlPath, html).text();
+    return rawVerses.push(verse);
   });
-  Promise.all(allChapters).then((chapters) => console.log(chapters[0].length)); //Array of books with array of chapters
+  verses = rawVerses.map((verse) => verse.trim());
+
+  let numberedVerses = {};
+  for (let i = 0; i < verseNumbers.length; i++) {
+    const number = verseNumbers[i];
+    numberedVerses[number] = verses[i];
+  }
+  console.log(JSON.stringify(numberedVerses));
 });
