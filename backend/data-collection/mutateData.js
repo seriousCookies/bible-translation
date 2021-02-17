d26102d0d601e76f8909d64877d8730ad3fe0023
Na Wei <na.wei95@hotmail.com>
2021-02-17 14:37:48 +0100

    getting the correct order

--- backend/data-collection/mutateData.js
+++ backend/data-collection/mutateData.js
@@ -1,5 +1,93 @@
 const rp = require("request-promise");
-const cheerio = require("cheerio");
+const $ = require("cheerio");
+const fs = require("fs");
+const readline = require("readline");
+const baseURL = "https://www.christunite.com/";
+
+const rl = readline.createInterface({
+  input: fs.createReadStream("./ot.txt"),
+  output: process.stdout,
+});
+const NTrl = readline.createInterface({
+  input: fs.createReadStream("./nt.txt"),
+  output: process.stdout,
+});
+
+const bookOrder = [];
+rl.on("line", (line) => {
+  const lineRegex = /^(?<overallOrder>[\d.]*)\s(?<english>[\w ]*)/gim;
+  let bookoverallOrder = lineRegex.exec(line);
+  bookOrder.push(JSON.parse(JSON.stringify(bookoverallOrder.groups)));
+});
+NTrl.on("line", (line) => {
+  const lineRegex = /^(?<testamentOrder>[\d.]*)\s(?<english>[\w ]*)/gim;
+  let bookoverallOrder = lineRegex.exec(line);
+  let bookInfo = JSON.parse(JSON.stringify(bookoverallOrder.groups));
+  bookInfo.overallOrder =
+    parseInt(bookInfo.testamentOrder.replace(".", "")) + 39;
+  bookOrder.push(bookInfo);
+});
+
+const url =
+  "https://www.christunite.com/index.php/chinese-bible/chinesepinyinbible";
+
+const getHref = (parseHtml, html, array) => {
+  return Array.from($(parseHtml, html)).map((book) =>
+    array.push(book.attribs.title)
+  );
+};
+const eachBook = [
+  "#s5_component_wrap_inner > div.item-page > div > div > table > tbody > tr > td > table > tbody > tr > td > a",
+  "#s5_component_wrap_inner > div.item-page > div > div > table > tbody > tr > td > a",
+  "#s5_component_wrap_inner > div.item-page > div > div > table > tbody > tr > td > p > a",
+];
+
+const getData = (url, parseHtml, getHref) =>
+  rp(url)
+    .then((html) => {
+      const rawURLs = [];
+      parseHtml.map((string) => getHref(string, html, rawURLs));
+      return Array.from(new Set(rawURLs));
+    })
+    .catch((err) => {
+      console.log("error:", url, "this is printing here", err);
+    });
+
+getData(url, eachBook, getHref).then((data) => {
+  const books = [];
+  const regex = /[\n]?(?<code>\w*\*?)\s(?<ch>[\u4E00-\u9FCC]*)\s\((?<py>[\w]*)\)\s(?<en>[\w ]*)/gim;
+  data = data
+    .join("\n")
+    .replace(regex, "$<code>, $<ch>, $<py>, $<en>§§")
+    .split("§§")
+    .map((bookString) => {
+      if (bookString === "") {
+        return;
+      } else {
+        const splitBookString = bookString.split(",");
+        const bookDetails = {
+          code: splitBookString[0].replace(/\*/gi, ""),
+          chinese: splitBookString[1],
+          pinyin: splitBookString[2] && splitBookString[2].replace(/\d/gi, ""),
+          english: splitBookString[3].trim(),
+        };
+        return books.push(bookDetails);
+      }
+    });
+  let a = books.map((book) => {
+    let bibleBooks = {
+      ...book,
+      ...bookOrder.filter((line) => line.english === book.english)[0],
+    };
+    bibleBooks.overallOrder <= 39
+      ? ((bibleBooks.testament = "OldTestament"),
+        (bibleBooks.testamentOrder = bibleBooks.overallOrder))
+      : (bibleBooks.testament = "NewTestament");
+    return bibleBooks;
+  });
+
+  console.log(a, "here now");
+});
 
 const MongoClient = require("mongodb").MongoClient;
 const uri = process.env.MONGODB_ATLAS_CONNECTION_STRING;
@@ -22,5 +110,3 @@ const updateDocument = async (documentURL) => {
     process.exit();
   }
 };
-
-//.parentElement.href).match(/\/([\d\-\w]*)\/$/)[1]==="chuangshiji")
