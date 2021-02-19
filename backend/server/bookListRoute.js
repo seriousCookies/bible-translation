8724ab42241d3a50146d5ec4b8c5d5b3ad3af1c7
Na Wei <na.wei95@hotmail.com>
2021-02-19 12:05:42 +0100

    refactoring code, mongodb connected

--- backend/server/bookListRoute.js
+++ backend/server/bookListRoute.js
@@ -1,50 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const mongodb = require("mongodb");
-const uri = process.env.MONGODB_ATLAS_CONNECTION_STRING;
+const MongoDB = require("../db/mongodb");
 
 router.get("/", (req, res) => {
-  mongodb.MongoClient.connect(
-    uri,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    },
-    async (error, client) => {
-      if (error) {
-        console.log(error);
-        process.exit(1);
-      }
-      const list = [];
-      const database = client.db("bible");
-      const collection = database.collection("bibleInfo");
-      const query = { overallOrder: { $gt: 0 } };
-      const options = {
-        sort: { overallOrder: 1 },
-        projection: {
-          _id: 0,
-          overallOrder: 1,
-          pinyin: 1,
-          chinese: 1,
-          english: 1,
-          testament: 1,
-        },
-      };
-      const cursor = collection.find(query, options);
-      if ((await cursor.count()) === 0) {
-        res.json("No documents found!");
-      }
-      await cursor.forEach((i) => {
-        collection.updateOne(
-          { english: i.english },
-          { $set: { overallOrder: parseInt(i.overallOrder) } }
-        );
-        list.push(i);
-      });
-      res.json(list);
+  MongoDB.connectDB(async (err) => {
+    if (err) console.log(err);
+    const db = MongoDB.getDB();
+    const list = [];
+    const collection = db.collection("bibleInfo");
+    const query = { overallOrder: { $gt: 0 } };
+    const options = {
+      sort: { overallOrder: 1 },
+      projection: {
+        _id: 0,
+        overallOrder: 1,
+        pinyin: 1,
+        chinese: 1,
+        english: 1,
+        testament: 1,
+      },
+    };
+    const cursor = collection.find(query, options);
+    if ((await cursor.count()) === 0) {
+      res.json("No documents found!");
     }
-  );
+    await cursor.forEach((i) => {
+      // collection.updateOne(
+      //   { english: i.english },
+      //   { $set: { overallOrder: parseInt(i.overallOrder) } }
+      // );
+      list.push(i);
+    });
+    res.json(list);
+  });
 });
 module.exports = router;
