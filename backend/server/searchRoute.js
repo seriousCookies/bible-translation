8358d27cc501586c1802fc34ecdc425fd66dd7a0
Na Wei <na.wei95@hotmail.com>
2021-02-19 14:34:01 +0100

    refactoring code- cleaning up switch statement

--- backend/server/searchRoute.js
+++ backend/server/searchRoute.js
@@ -1,3 +1,4 @@
+const { raw } = require("body-parser");
 const express = require("express");
 const router = express.Router();
 const MongoDB = require("../db/mongodb");
@@ -13,55 +14,40 @@ const getData = (database, q, verse) =>
 
 router.get("/", (req, res) => {
   const { translation, book, chapter, verse } = req.query;
-  let language;
-  const chinese = {};
-  const pinYin = {};
-  const english = {};
-  switch (translation) {
-    case "en":
-      language = "english-bible";
-      english.verse = "esvVerse";
-      break;
-    case "ch":
-      language = "chinese-bible";
-      chinese.verse = "ChineseVerse";
-      pinYin.verse = "pinYinVerse";
-      break;
-    default:
-      language = "chinese-bible";
-      break;
-  }
+  const query = {};
+  book && (query.BookName = book);
+  chapter && (query.BookChapter = chapter);
+  verse && (query.verseNumber = verse);
+  const rawData = [];
+  const collection = {};
   MongoDB.connectDB(async (err) => {
     if (err) console.log(err);
     const db = MongoDB.getDB();
-    pinYin.dbCollection = db.collection("pinyin-ch-bible-chapter");
-    chinese.dbCollection = db.collection("ch-bible-chapter");
-    english.dbCollection = db.collection("esv-en-bible-chapter");
-    const query = {};
-    book && (query.BookName = book);
-    chapter && (query.BookChapter = chapter);
-    verse && (query.verseNumber = verse);
-    let rawData = [];
-
     switch (translation) {
       case "en":
-        const esvRes =
-          english.dbCollection &&
-          (await getData(english.dbCollection, query, english.verse));
+        collection.english = db.collection("esv-en-bible-chapter");
+        const esvRes = await getData(collection.english, query, "esvVerse");
         rawData.push(esvRes);
         break;
-
       case "ch":
-        const chineseRes =
-          chinese.dbCollection &&
-          (await getData(chinese.dbCollection, query, chinese.verse));
-        const pinYinRes =
-          pinYin.dbCollection &&
-          (await getData(pinYin.dbCollection, query, pinYin.verse));
+        collection.chinese = await db.collection("ch-bible-chapter");
+        collection.pinyin = await db.collection("pinyin-ch-bible-chapter");
+        const pinYinRes = await getData(
+          collection.pinyin,
+          query,
+          "pinYinVerse"
+        );
         rawData.push(pinYinRes);
+        const chineseRes = await getData(
+          collection.chinese,
+          query,
+          "ChineseVerse"
+        );
         rawData.push(chineseRes);
+
         break;
       default:
+        rawData.push("choose a translation");
         break;
     }
     const data = await rawData
